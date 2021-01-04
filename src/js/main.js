e80f61eae536d8e5a93a04ae08e5bdff7677a3b1
Andrew <andreyalexandrovich.k@gmail.com>
2021-01-04 21:06:42 +0200

    Step_5: added checking number/text inputs

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,16 @@
 import modals from './modules/modals';
 import slider from './modules/slider';
 import forms from './modules/forms';
+import showMoreStyles from './modules/showMoreStyles';
+import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
     slider('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     slider('.main-slider-item', 'vertical');
     forms();
+    showMoreStyles('.button-styles', '.styles-2');
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
 });
---
+++ src/js/modules/checkTextInputs.js
@@ -0,0 +1,19 @@
+const checkTextInputs = (selector) => {
+    const inputs = document.querySelectorAll(selector);
+
+    inputs.forEach(input => {
+        input.addEventListener('input', () => {
+            if (input.value.match(/[a-z]/ig)) {
+                input.value = '';
+            }
+        });
+
+        input.addEventListener('keypress', (e) => {
+            if (e.key.match(/[^а-яё 0-9]/ig)) {
+                e.preventDefault();
+            }
+        });
+    });
+};
+
+export default checkTextInputs;
